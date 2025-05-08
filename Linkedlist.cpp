1339aa4cc6565c99c8740241050c22ea793de56f
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-08 11:46:11 +0700

    Menambahkan logika untuk menu 4

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -163,4 +163,27 @@ class singleLinkedList
             {
                 mhs.traverse();
             }
+            break;
+            case '4':
+            {
+                if (mhs.singleLinkedlistEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                Node *previous, *current;
+                cout << endl
+                     << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, &current) == false)
+                    cout << endl
+                         << "Data tidak ditemukan\n" << endl;
+                else
+                {
+                    cout << endl
+                         << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
             

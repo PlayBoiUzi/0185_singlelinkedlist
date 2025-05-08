4e1047a1374c434ebea8083e432f25eac1fb4257
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-08 11:40:02 +0700

    Menambahkan fungsi delNode

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -78,3 +78,19 @@ class singleLinkedList
            return (*current != NULL);
        }
    
+       bool delNode(int nim)
+       {
+           Node *current, *previous;
+           if (!Search(nim, &previous, &current))
+               return false;
+           
+           if (current == START)
+               START = START->next;
+           else
+               previous->next = current->next;
+           
+           delete current;
+           return true;
+       }
+   
+       

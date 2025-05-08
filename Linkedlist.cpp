872534180aa60dcf3ac3acb1015cc194c5d2f407
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-08 11:33:25 +0700

    Menambahkan Fungsi addNode

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -13,3 +13,32 @@ class singleLinkedList
 {
     Node *START;
 
+    public:
+    singleLinkedList()
+       {
+           START = NULL;
+       }
+   
+   
+       void addNode()
+       {
+           int nim;
+           cout << "\nMasukkan Nomor Mahasiswa: ";
+           cin >> nim;
+   
+           Node *nodeBaru = new Node;
+           nodeBaru->noMhs = nim;
+   
+           if (START == NULL || nim <= START->noMhs)
+           {
+               if ((START != NULL && (nim == START->noMhs)))
+               {
+                   cout << "\nDuplikat noMhs tidak diijinkan\n";
+                   return;
+               }
+               nodeBaru->next = START;
+               START = nodeBaru;
+               return;
+           }
+   
+           
